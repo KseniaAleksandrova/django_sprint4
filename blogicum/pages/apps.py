153b3c8fec69baad95b8c4f319a99b15a356531c
KseniaAleksandrova <ksushka.alex05@gmail.com>
2024-12-25 23:20:07 +0300

    Update apps.py

--- blogicum/pages/apps.py
+++ blogicum/pages/apps.py
@@ -1,6 +1,8 @@
+""" Файл служит для определения конфигурации приложения pages. """
+
 from django.apps import AppConfig
 
-
+""" Настройки для приложения, обрабатывающего статические и вспомогательные  страницы """
 class PagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pages'
