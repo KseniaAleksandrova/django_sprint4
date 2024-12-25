44dbbae3e5b80bac21b51529dea22748333a6999
KseniaAleksandrova <ksushka.alex05@gmail.com>
2024-12-25 23:23:43 +0300

    Update apps.py

--- blogicum/users/apps.py
+++ blogicum/users/apps.py
@@ -1,6 +1,9 @@
+""" Файл регистрирует приложение users в проекте. 
+Он используется для настройки и определения базовых параметров приложения(имя приложения, настройки автоматических первичных ключей). """
+
 from django.apps import AppConfig
 
-
+""" Настройка приложения users. """
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
