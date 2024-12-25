ccf4509c28d6bb2e1ed9685d69b7fd6bb60d6378
KseniaAleksandrova <ksushka.alex05@gmail.com>
2024-12-25 23:20:54 +0300

    Update urls.py

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,10 +1,13 @@
+""" Файл определяет маршруты для приложения pages. 
+    Он связывает URL-адреса с представлениями, которые отображают статические страницы. """
+
 from django.urls import path
 
 from . import views
 
-app_name = 'pages'
+app_name = 'pages' #пространство имен для URL
 
-urlpatterns = [
+urlpatterns = [ #список маршрутов
     path('about/', views.AboutView.as_view(), name='about'),
     path('rules/', views.RulesView.as_view(), name='rules'),
 ]
