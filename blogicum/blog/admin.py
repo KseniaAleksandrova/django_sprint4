6ab6d45df8cd56b9b4286558b8374b400e0b3a3d
KseniaAleksandrova <ksushka.alex05@gmail.com>
2024-12-25 22:56:24 +0300

    Update admin.py

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,14 +1,19 @@
+""" Файл admin.py содержит настройки административного интерфейса для моделей
+Post, Category, Location и Comment. Он настраивает отображение, 
+редактирование и фильтрацию данных в админке Django. """
+
 from django.contrib import admin
 
 from .models import Category, Comment, Location, Post
 
-LENGTH_STRING = 50
-NUMBER_OF_POSTS = 10
+LENGTH_STRING = 50 #Максимальная длина строки для отображения
+NUMBER_OF_POSTS = 10 #Количество постов на странице
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    """Кастомизация админки для модели Post."""
+    """ Кастомизация админки для модели Post.
+    Включает настройки отображения, редактирования, поиска и фильтрации постов. """
 
     list_display = (
         'title',
@@ -32,14 +37,18 @@ class PostAdmin(admin.ModelAdmin):
     list_per_page = NUMBER_OF_POSTS
 
     @staticmethod
-    @admin.display(description='Текст')
+    @admin.display(description='Текст') #Описание поля в админке
     def text_short(object: Post) -> str:
+        """ Возвращает сокращённый текст поста, 
+        чтобы избежать длинных текстов в списке админки. """
         return f'{object.text[:LENGTH_STRING]}...'
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-
+    """ Кастомизация админки для модели Category.
+    Включает настройки отображения, редактирования и фильтрации категорий. """
+    
     list_display = (
         'title',
         'description_short',
@@ -59,12 +68,15 @@ class CategoryAdmin(admin.ModelAdmin):
     @staticmethod
     @admin.display(description='Описание')
     def description_short(object: Category) -> str:
+        """ Возвращает сокращённое описание категории, 
+        чтобы избежать длинных описаний в списке админки. """    
         return f'{object.description[:LENGTH_STRING]}...'
 
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    """Кастомизация админки для модели Location."""
+    """ Кастомизация админки для модели Location.
+    Управление отображением и фильтрацией локаций. """
 
     list_display = (
         'name',
@@ -78,7 +90,8 @@ class LocationAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    """Кастомизация админки для модели Comment."""
+    """Кастомизация админки для модели Comment.
+    Управление отображением и фильтрацией комментариев."""
 
     list_display = (
         'text',
