427c1f2d3f592405d70838a3db4efbecad870f6d
KseniaAleksandrova <ksushka.alex05@gmail.com>
2024-12-25 23:17:40 +0300

    Update urls.py

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,40 +1,31 @@
-"""blogicum URL Configuration
+""" Файл задает основные маршруты (URL) всего проекта. 
+Он связывает URL-адреса с представлениями, 
+а также определяется как обрабатывать  маршруты в приложении. """
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 
+""" Основной список маршрутов проекта """
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), #URL для административной панели
     path('', include('blog.urls')),
     path('pages/', include('pages.urls')),
     path('auth/', include('django.contrib.auth.urls')),
     path('auth/registration/', include('users.urls')),
 ]
+""" Настройка обработчиков ошибок """
+handler403 = 'pages.views.csrf_failure' #ошибка 403 (отказ в доступе)
 
-handler403 = 'pages.views.csrf_failure'
+handler404 = 'pages.views.page_not_found' #ошибка 404 (страница не найдена)
 
-handler404 = 'pages.views.page_not_found'
+handler500 = 'pages.views.internal_error' #ошибка 500 (внутренняя ошибка сервера)
 
-handler500 = 'pages.views.internal_error'
-
+#доп настройка для режима DEBUG
 if settings.DEBUG:
     import debug_toolbar
-    # Добавить к списку urlpatterns список адресов из приложения debug_toolbar:
+    #добавление к списку urlpatterns список адресов из приложения debug_toolbar:
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
-
+#добавление маршрутов для обработки медиа-файлов
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
