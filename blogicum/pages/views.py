8ae619d83d065eaa5e21bfc19c6b3a0645de78bb
KseniaAleksandrova <ksushka.alex05@gmail.com>
2024-12-25 23:22:45 +0300

    Update views.py

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,22 +1,25 @@
+""" Файл содержит представления для обработки запросов и возврата страниц. """
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
+""" AboutView и RulesView нужны для отображения страниц "О проекте" и "Правила". """
 class AboutView(TemplateView):
-    template_name = 'pages/about.html'
+    template_name = 'pages/about.html' #отображает статическую страницу "О проекте"
 
 
 class RulesView(TemplateView):
-    template_name = 'pages/rules.html'
+    template_name = 'pages/rules.html' #отображает статическую страницу "Правила"
 
-
+""" Обработчики ошибок нужны для возврата пользовательских страниц при возникновении ошибок. """
 def page_not_found(request, *args, **kwargs):
-    return render(request, 'pages/404.html', status=404)
+    return render(request, 'pages/404.html', status=404) #отображает пользовательскую страницу ошибки 404
 
 
 def csrf_failure(request, *args, **kwargs):
-    return render(request, 'pages/403csrf.html', status=403)
+    return render(request, 'pages/403csrf.html', status=403) #отображает страницу ошибки 403
 
 
 def internal_error(request, *args, **kwargs):
-    return render(request, 'pages/500.html', status=500)
+    return render(request, 'pages/500.html', status=500) #отображает страницу ошибки 500
