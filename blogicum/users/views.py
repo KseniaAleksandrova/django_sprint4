a8c134b23d8a74e85cea0dd8678dde5e43828c42
KseniaAleksandrova <ksushka.alex05@gmail.com>
2024-12-25 23:24:42 +0300

    Update views.py

--- blogicum/users/views.py
+++ blogicum/users/views.py
@@ -1,9 +1,12 @@
+""" Определяет логику регистрации пользователя через представление. """
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 
+""" Представление для регистрации нового пользователя. """
 class UserCreateView(CreateView):
     model = get_user_model()
     template_name = 'registration/registration_form.html'
