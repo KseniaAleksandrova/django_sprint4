7b24d564a0ea2c717100ffd8ebf3562e000dff9c
KseniaAleksandrova <ksushka.alex05@gmail.com>
2024-12-26 08:35:37 +0300

    Update apps.py

--- blogicum/blog/apps.py
+++ blogicum/blog/apps.py
@@ -1,9 +1,11 @@
 """ Файл apps.py определяет настройки для приложения blog. """
 
-from django.apps import AppConfig
+from django.apps import AppConfig # Импортируем базовый класс AppConfig из Django для настройки приложения
 
 
-class BlogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog'
+
+class BlogConfig(AppConfig): # Определяем класс BlogConfig, который наследует от AppConfig
+
+    default_auto_field = 'django.db.models.BigAutoField' # Указываем тип поля по умолчанию для автоматического увеличения (BigAutoField)
+    name = 'blog' # Задаем имя приложения
     verbose_name = 'Блог'
